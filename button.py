c7c395d0bd686383e63db9a3fcca8bc5c176f1ec
AloisCKaufmann <alois.kaufmann@gmail.com>
2017-04-03 13:58:43 +0200

    Create button.py

---
+++ button.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+import time
+from time import sleep
+from picamera import PiCamera
+import datetime
+import time
+
+date = datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+camera = PiCamera()
+
+while True:
+        input_state = GPIO.input(18)
+        if input_state == False:
+                print("button pressed")
+                time.sleep(0.2)
+                camera.rotation = 180
+                camera.start_preview()
+                if input_state == False:
+                    camera.start_preview ()
+                    camera.preview_fullscreen = True
+                    camera.preview_alpha = 255
+                    time.sleep(45)  
+                    print("Foto")
+                    camera.capture('/home/pi/Desktop/'+ date + '.jpg' )
+                    camera.stop_preview()
+                        
